7480b58104a72dc496c1973369d38a2816533a6b
Abhinandan <dwabhi2006@gmail.com>
2026-01-27 12:42:53 +0530

    user register with file handling

--- src/Controllers/UserRegister.js
+++ src/Controllers/UserRegister.js
@@ -1,9 +1,58 @@
 import AsyncHandler from '../Utils/AsyncHandler.js';
+import User from '../Models/userModel.js';
+import Showerror from '../Utils/Showerror.js';
+import Uploadoncloudinary  from '../Utils/CloudinaryUpload.js';
+import apiresponse from '../Utils/ApiResponse.js';
 
 const Registeruser   = AsyncHandler( async(req, res) => {
 
+    const { username, fullname, email, password } = req.body;
+    if ( [username, fullname, email, password].some( (field)=>{
+        return field.trim() === ""; }) ) 
+    {
+        throw new Showerror( 400, "All fields are required" );
+    }
 
+    // check if user already exists
+    const existingUser = await user.findOne({  $or : [ { email }, { username } ] });
+    if  ( existingUser ) {
+        throw new Showerror( 400, "User already exists with this email or username" );
+    }
 
-    res.status(200).json({message: "User Registered Successfully"});
+    // create new user 
+    const avatarlocalpath = await req.files?.avatar[0]?.path;
+    const coverImagelocalpath = await req.files?.coverImage[0]?.path;
+
+    if ( !avatarlocalpath ) {
+        throw new Showerror( 400, "Avatar image is required" );
+    }
+
+    const avatar = await Uploadoncloudinary(avatarlocalpath);
+    const coverImage = coverImagelocalpath ? await Uploadoncloudinary(coverImagelocalpath) : null;
+
+    if ( !avatar ) {
+        throw new Showerror( 500, "Error uploading avatar image" );
+    }
+
+    // create new user in database
+    const newuser = await User.create({ 
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email, 
+        password,
+        username: username.toLowerCase()
+    })
+    if ( !newuser ) {
+        throw new Showerror( 500, "Error creating user" );
+    }
+
+    const removeuserfields  = await newuser.findById(newuser._id).select("-password -refressToken");
+    if ( !removeuserfields ) {
+        throw new Showerror( 500, "Error retrieving user data" );
+    }
+
+    return res.status(201).json(new apiresponse( 201, "User registered successfully", removeuserfields ));
 })
+
 export default Registeruser;
--- src/Routes/User.route.js
+++ src/Routes/User.route.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import Registeruser from "../Controllers/userRegister.js";
+import Upload from "../Middlewares/multer.middleware.js";
 
 const router = Router();
-router.route("/register").post(Registeruser);
+router.route("/register").post(
+    Upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1    
+        }
+    ]), Registeruser);
 
 export default router;
