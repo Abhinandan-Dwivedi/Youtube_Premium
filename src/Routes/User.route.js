59bdfdd43f7b72308ac7f0b29c2d6a21967c5f9c
Abhinandan <dwabhi2006@gmail.com>
2026-02-10 20:01:36 +0530

    set routes of user

--- src/Routes/User.route.js
+++ src/Routes/User.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { Registeruser, Logout, Login } from "../Controllers/userRegister.js";
+import {
+    Registeruser,
+    Logout,
+    Login,
+    changecurrentpassword,
+    getcurrentuser,
+    updateuserdetails,
+    updateAvatarimg,
+    updateCoverimg,
+    getUserChannelProfile,
+    getWatchHistory
+  } 
+from "../Controllers/userRegister.js";
 import { Upload } from "../Middlewares/multer.middleware.js";
 import Authstatus from "../Middlewares/Authstatus.middleware.js";
 
@@ -20,5 +32,19 @@ router.route("/register").post(
 router.route("/login").post(Login);
 
 router.route("/logout").post(Authstatus, Logout);
+
+router.route("/password-change").post(Authstatus, changecurrentpassword);
+
+router.route("/current-user").get(Authstatus, getcurrentuser);
+
+router.route("/update-Account").patch(Authstatus, updateuserdetails);
+
+router.route("/update-avatar").patch(Authstatus, Upload.single("avatar"), updateAvatarimg);
+
+router.route("/update-cover").patch(Authstatus, Upload.single("coverImage"), updateCoverimg);
+
+router.route("/c/:username").get(getUserChannelProfile);
+
+router.route("/watch-history").get(Authstatus, getWatchHistory);
 
 export default router;
