d14ffe608ea6595e62ebb94a0d8ca674cdd2d0a6
Abhinandan <dwabhi2006@gmail.com>
2026-02-20 12:58:03 +0530

    routing in app

--- src/app.js
+++ src/app.js
@@ -14,6 +14,23 @@ app.use( express.static("public") );
 app.use( cookie());
 
 import UserRouter from "./Routes/User.route.js";
+import TweetRouter from "./Routes/tweet.route.js";
+import SubscriberRouter from "./Routes/Subscriber.route.js";
+import DashboardRouter from "./Routes/dashboard.route.js";
+import LikesRouter from "./Routes/likes.route.js";
+import HealthcheckRouter from "./Routes/healthcheck.route.js";
+import CommentRouter from "./Routes/Comment.route.js";
+import VideoRouter from "./Routes/Video.route.js";
+import PlaylistRouter from "./Routes/Playlist.route.js";
+
 app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/tweet", TweetRouter);
+app.use("/api/v1/subscriber", SubscriberRouter);
+app.use("/api/v1/dashboard", DashboardRouter);
+app.use("/api/v1/likes", LikesRouter);
+app.use("/api/v1/healthcheck", HealthcheckRouter);
+app.use("/api/v1/comment", CommentRouter);
+app.use("/api/v1/video", VideoRouter);
+app.use("/api/v1/playlist", PlaylistRouter);
 
 export default app;
